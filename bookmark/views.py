150f98f569d9f74ac28215d7675b20993eed98d5
ovoHJ <s2018w01@gmail.com>
2020-05-21 10:53:12 +0900

    urls detail/<int:pk>

--- bookmark/views.py
+++ bookmark/views.py
@@ -15,5 +15,5 @@ class BookmarkCreateView(CreateView):  # bookmark_form.html
     template_name_suffix = '_create'  # bookmark_create.html
     success_url = reverse_lazy('bookmark:list')  # == {% 'bookmark:list' %}
 
-class BookmarkCreateView(DetailView):
+class BookmarkDetailView(DetailView):
     model = Bookmark
